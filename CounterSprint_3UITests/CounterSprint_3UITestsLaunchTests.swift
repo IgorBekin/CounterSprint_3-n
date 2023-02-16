02b42786d0250761c22e5ff6e964277a42be8bee
igorBekin <igor_bekin@mail.ru>
2023-02-16 14:04:21 +0300

    Initial Commit

---
+++ CounterSprint_3UITests/CounterSprint_3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CounterSprint_3UITestsLaunchTests.swift
+//  CounterSprint_3UITests
+//
+//  Created by Игорь Бекин on 16.02.2023.
+//
+
+import XCTest
+
+final class CounterSprint_3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
